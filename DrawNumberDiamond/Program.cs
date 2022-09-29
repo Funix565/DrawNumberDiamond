b35b797983dcb6d340d97446c3371ab0e6f3fb77
Funix565 <denkin.dmitriy@gmail.com>
2022-09-29 15:53:22 +0300

    Add Solution and README.MD

--- DrawNumberDiamond/Program.cs
+++ DrawNumberDiamond/Program.cs
@@ -4,7 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DrawDiamond(9);
+        }
+
+        private static void DrawDiamond(int num)
+        {
+            int[,] res = new int[num, num];
+
+            int i = 0;
+
+            // The starting number of symbols to draw.
+            int count12 = num / 2;
+
+            // First loop for the upper half of the whole square
+            for (i = 0; i < num / 2; ++i)
+            {
+                // Draw ones before the middle
+                for (int j = 0; j < count12; ++j)
+                {
+                    res[i, j] = 1;
+                }
+
+                // Draw twos after the middle
+                // j = num - 1 -- because we draw from the end to the middle
+                // j > num - 1 - count12 -- the stop position becomes right-oriented
+                for (int j = num - 1; j > num - 1 - count12; --j)
+                {
+                    res[i, j] = 2;
+                }
+
+                // Reduce the number of symbols to print in the next iteration
+                --count12;
+            }
+
+            // Skip the middle line of zeros
+            ++i;
+
+            // Here we have another (but yet still the same) approach
+            // The starting number of symbols to draw.
+            int count34 = 1;
+
+            for (; i < num; ++i)
+            {
+                // Simply draw the exact count of symbols
+                for (int j = 0; j < count34; ++j)
+                {
+                    res[i, j] = 3;
+                }
+
+                // Draw from the end to the middle
+                for (int j = num - 1; j > num - 1 - count34; --j)
+                {
+                    res[i, j] = 4;
+                }
+
+                // Increase the number of symbols to print in the next iteration
+                ++count34;
+            }
+
+            // Print 2d array
+            for (int k = 0; k < res.GetLength(0); k++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    Console.Write(res[k, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # DrawNumberDiamond
+
+The user enters an odd number. The program prints a two-dimensional array according to this principle:
+
+```
+n = 5;
+11022
+10002
+00000
+30004
+33044
+
+n = 9;
+111102222
+111000222
+110000022
+100000002
+000000000
+300000004
+330000044
+333000444
+333304444
+
+```
